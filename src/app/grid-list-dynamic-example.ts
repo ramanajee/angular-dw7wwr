23094dfc34e48dd45eafc8ad0d46172152a4940e
ramanajee <ramanajee040@gmail.com>
2020-03-14 01:12:23 +0530

    2 files modified

--- src/app/grid-list-dynamic-example.ts
+++ src/app/grid-list-dynamic-example.ts
@@ -2,16 +2,14 @@ import {Component} from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { Observable,timer,interval} from 'rxjs'
 import{ take ,map} from 'rxjs/operators';
+declare var location:any;
 @Component({
   selector: 'grid-list-dynamic-example',
   templateUrl: 'grid-list-dynamic-example.html',
   styleUrls: ['grid-list-dynamic-example.css'],
 })
 export class GridListDynamicExample {
-  bNumber='';
   onTimeout(event:any):void{
-      if(event as boolean){
-        this.bNumber = '';
-      }
+      location.reload();
   }
 }
